f58e521adda7de251b4f1bbfcdf8ad5bb844d492
Rio0628 <mariomdomenech@gmail.com>
2021-12-11 13:26:01 -0500

    Created View
    - Created the basic view of the application with the history view.
    - basic main view is where the user will be able to play the game as well as choose a game mode.
    - The history view serves as a way for the user to go back on their moves.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { BsFillArrowLeftCircleFill } from 'react-icons/bs'
 import Board from './components/Board';
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <Board />
+
+      <div className='mainGameView'>
+        <div className='navBar'>
+          <p className='winnerHeading'>Winner: Player 1</p>
+
+          <p className='historyTrigger'>History</p>
+        </div>
+
+        <Board />
+
+        <p className='nextPlayerHdng'>Next Player: Player 1</p>
+      </div>
+
+      <div className='historyView'>
+        <div className='heading-btnCntr'>
+          <div className='closeViewBtn'><BsFillArrowLeftCircleFill className='icon'/></div>
+
+          <p className='historyViewHdng'>History</p>
+        </div>
+
+        <div className='indPastMovesCntr'>
+          <p className='indPastMove'>Go to move #3</p>
+        </div>
+      </div>
+
     </div>
   );
 }
